01a4cc4987d2aa4f4d6945f539c490589435a511
IsidoreTam <59669931+IsidoreTam@users.noreply.github.com>
2020-10-26 18:53:06 -0600

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+let images = ['mountain1.jpg', 'mountain2.jpg', 'mountain3.jpg'];
+let currentImg = 0;
+let cImage = document.querySelector('.carousel>img');
+
+window.addEventListener('load', function(e){
+    cImage.src = 'images/' + images[0]; 
+
+    document.querySelector('.carousel').addEventListener('click', function(evt){
+        let target = evt.target;
+
+        if (currentImg > 2){
+            currenImg = 0;
+        } else if (currentImg < 0){
+            currentImg = 2;
+        }
+        
+        if (target.classList.contains('control')) {
+
+            if (target.classList.contains('next')) {
+                currentImg += 1;
+            } else if (target.classList.contains('prev')){
+                currentImg -= 1;
+            }
+
+            cImage.src = 'images/' + images[currentImg];
+        }
+    })
+})
